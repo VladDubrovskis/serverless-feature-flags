22fc714b827c39cf28fcc7417ad20103510890d4
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-03-13 19:21:24 +0000

    Update handler to Scan the DynamoDB table and return the Items as a response body

--- handler.js
+++ handler.js
@@ -1,16 +1,21 @@
 'use strict';
+const aws = require('aws-sdk');
+
+const params = {
+  TableName : 'featureFlags',
+};
+const docClient = new aws.DynamoDB.DocumentClient();
+const response = {
+  statusCode: 200
+};
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  docClient.scan(params, function(err, data) {
+    if (err) {
+      response.body = JSON.stringify(err);
+      response.code = 500;
+    };
+    response.body = data.Items;
+    callback(null, response);
+  });
 };
