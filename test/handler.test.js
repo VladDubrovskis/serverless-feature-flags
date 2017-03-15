203da9ad16775db2deaba7135be7aec931f39bb0
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-03-15 09:01:37 +0000

    Add first happy path test

--- test/handler.test.js
+++ test/handler.test.js
@@ -1,8 +1,24 @@
-var assert = require('assert');
-describe('Sanity check', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+const assert = require('assert');
+const handler = require('../handler.js');
+const dynamoResponse = require('./mocks/dynamo-response');
+const AWS = require('aws-sdk-mock');
+const sinon = require('sinon');
+
+describe('Feature flags endpoint', function() {
+  before(() => {
+    AWS.mock('DynamoDB.DocumentClient', 'scan', function(params, callback) {
+      callback(null, dynamoResponse);
     });
+  })
+
+  it('should return 200 with data from DynamoDB', function() {
+    const callback = sinon.stub();
+    handler.hello(undefined, undefined, callback);
+    assert.equal(callback.firstCall.args[1].statusCode, 200);
+    assert.deepEqual(callback.firstCall.args[1].body, dynamoResponse.Items);
   });
+
+  after(() => {
+    AWS.restore('DynamoDB.DocumentClient');
+  })
 });
