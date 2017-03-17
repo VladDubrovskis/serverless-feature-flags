f711c95d2e30c38e7c29ea2ec507fd7ec3defee9
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-03-17 08:54:57 +0000

    Add a test to verify the handler calls the response transform

--- test/handler.test.js
+++ test/handler.test.js
@@ -3,22 +3,35 @@ const handler = require('../handler.js');
 const dynamoResponse = require('./mocks/dynamo-response');
 const AWS = require('aws-sdk-mock');
 const sinon = require('sinon');
+const responseTransform = require('../response-transform');
 
 describe('Feature flags endpoint', function() {
+  let responseTransformResponse;
+  let responseTransformStub;
+
   before(() => {
     AWS.mock('DynamoDB.DocumentClient', 'scan', function(params, callback) {
       callback(null, dynamoResponse);
     });
+
+    responseTransformResponse = {
+        "test2": true,
+        "test": false
+    };
+
+    responseTransformStub = sinon.stub(responseTransform, 'transform').returns(responseTransformResponse);
   })
 
   it('should return 200 with data from DynamoDB', function() {
     const callback = sinon.stub();
     handler.hello(undefined, undefined, callback);
     assert.equal(callback.firstCall.args[1].statusCode, 200);
-    assert.deepEqual(callback.firstCall.args[1].body, dynamoResponse.Items);
+    assert.equal(responseTransformStub.callCount, 1);
+    assert.deepEqual(callback.firstCall.args[1].body, responseTransformResponse);
   });
 
   after(() => {
     AWS.restore('DynamoDB.DocumentClient');
+    responseTransformStub.reset();
   })
 });
