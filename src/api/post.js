1f434120d751a0dafa8cb81f545ca0fd241ef96d
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-03-21 13:44:06 +0000

    Add aws-sdk and make it write to the DynamoDB according to defined schema

--- src/api/post.js
+++ src/api/post.js
@@ -1,5 +1,40 @@
 'use strict';
+const aws = require('aws-sdk');
+const response = {
+  statusCode: 200,
+  body: "OK"
+};
+let payload;
+
+const errorResponse = (callback) => {
+  callback(null, {statusCode: 500, body: "Invalid request"});
+}
 
 module.exports.handler = (event, context, callback) => {
-  callback(null, { statusCode: 200, body: "Request succeeded"});
+
+  try {
+    payload = JSON.parse(event.body);
+  } catch (e) {
+    errorResponse(callback);
+  }
+
+  const newItem = {
+    "featureName": payload.featureName,
+    "state": payload.state
+  };
+
+  const params = {
+    TableName : 'featureFlags',
+    Item: newItem
+  };
+
+  const docClient = new aws.DynamoDB.DocumentClient();
+
+  docClient.put(params, (err, data) => {
+    if (err) {
+      response.body = JSON.stringify(err);
+      response.statusCode = 500;
+    };
+    callback(null, response);
+  });
 };
