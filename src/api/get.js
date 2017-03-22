36d90221bb3ea87bda8a2f56547e6893d10743f9
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-03-22 09:15:17 +0000

    Update GET endpoint - return Promise from AWS-SDK library

--- src/api/get.js
+++ src/api/get.js
@@ -6,17 +6,20 @@ module.exports.handler = (event, context, callback) => {
   const params = {
     TableName : 'featureFlags',
   };
+
   const docClient = new aws.DynamoDB.DocumentClient();
   const response = {
     statusCode: 200
   };
 
-  docClient.scan(params, (err, data) => {
-    if (err) {
+  return docClient.scan(params).promise()
+    .then((data) => {
+      response.body = JSON.stringify(responseTransform.transform(data.Items));
+      callback(null, response);
+    })
+    .catch((err) => {
       response.body = JSON.stringify(err);
       response.statusCode = 500;
-    };
-    response.body = JSON.stringify(responseTransform.transform(data.Items));
-    callback(null, response);
-  });
+      callback(null, response);
+    });
 };
