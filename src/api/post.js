0ea0a9d2e9f95f647551c3913102156d2c26521b
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-03-23 09:20:21 +0000

    Update POST handler to return Promis Fixes the 1 of 3 failing tests: “Feature flags POST endpoint should return 200 when payload is correct”

--- src/api/post.js
+++ src/api/post.js
@@ -1,40 +1,39 @@
 'use strict';
 const aws = require('aws-sdk');
-const response = {
-  statusCode: 200,
-  body: "OK"
-};
 let payload;
-
-const errorResponse = (callback) => {
-  callback(null, {statusCode: 500, body: "Invalid request"});
-}
 
 module.exports.handler = (event, context, callback) => {
 
-  try {
-    payload = JSON.parse(event.body);
-  } catch (e) {
-    errorResponse(callback);
-  }
+    try {
+        payload = JSON.parse(event.body);
+    } catch (e) {
+        callback(null, {
+            statusCode: 500,
+            body: "Invalid request"
+        });
+    }
 
-  const newItem = {
-    "featureName": payload.featureName,
-    "state": payload.state
-  };
-
-  const params = {
-    TableName : 'featureFlags',
-    Item: newItem
-  };
-
-  const docClient = new aws.DynamoDB.DocumentClient();
-
-  docClient.put(params, (err, data) => {
-    if (err) {
-      response.body = JSON.stringify(err);
-      response.statusCode = 500;
+    const newItem = {
+        "featureName": payload.featureName,
+        "state": payload.state
     };
-    callback(null, response);
-  });
+
+    const params = {
+        TableName: 'featureFlags',
+        Item: newItem
+    };
+
+    const docClient = new aws.DynamoDB.DocumentClient();
+
+    return docClient.put(params).promise().then(() => {
+        callback(null, {
+            statusCode: 200,
+            body: "OK"
+        });
+    }).catch((err) => {
+        callback(null, {
+            statusCode: 500,
+            body: JSON.stringify(err)
+        });
+    });
 };
