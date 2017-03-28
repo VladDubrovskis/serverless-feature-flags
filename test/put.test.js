a161e44fa94d9be023145bad1558f52a793effbd
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-03-28 08:58:34 +0100

    Update PUT endpoint test to expect a promise

--- test/put.test.js
+++ test/put.test.js
@@ -2,10 +2,13 @@ const assert = require('assert');
 const put = require('../src/api/put.js');
 const sinon = require('sinon');
 
-describe('Feature flags PUT endpoint', function() {
+describe('Feature flags PUT endpoint', function(done) {
   it('should return 200', function() {
     const callback = sinon.stub();
-    put.handler(undefined, undefined, callback);
-    assert.equal(callback.firstCall.args[1].statusCode, 200);
+    put.handler(undefined, undefined, callback).then(() => {
+      assert.equal(callback.firstCall.args[1].statusCode, 200);
+      done();
+    });
+
   });
 });
