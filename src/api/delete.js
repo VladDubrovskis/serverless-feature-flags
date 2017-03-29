37c035dbab2fcdd0dc4d3ee6eae163ea8a604b78
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-03-29 08:57:12 +0100

    Update DELETE handler to pass the test

--- src/api/delete.js
+++ src/api/delete.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports.handler = (event, context, callback) => {
-  
+  return new Promise((resolve, reject) => {
+    callback(null, { statusCode: 501, body: "Not Implemented"});
+    reject();
+  });
 };
