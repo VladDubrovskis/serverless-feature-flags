c87456a7b347718804f94e91a78c090ed9405e25
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-03-30 08:41:49 +0100

    Add PUT handler test to cover case when the item we want to update is not in DynamoDB

--- test/put.test.js
+++ test/put.test.js
@@ -1,13 +1,27 @@
 const assert = require('assert');
 const put = require('../src/api/put.js');
 const sinon = require('sinon');
+const AWS = require('aws-sdk-mock');
 
 describe('Feature flags PUT endpoint', () => {
+
+  afterEach(() => {
+    AWS.restore();
+  });
+
   it('should return 501', () => {
     const callback = sinon.stub();
     return put.handler(undefined, undefined, callback).catch(() => {
       assert.equal(callback.firstCall.args[1].statusCode, 501);
     });
+  });
 
+  it('should return 404 when the item is not found in DynamoDB', () => {
+    const callback = sinon.stub();
+    AWS.mock('DynamoDB.DocumentClient', 'get', Promise.resolve({}));
+    return put.handler(undefined, undefined, callback).catch(() => {
+      assert.equal(callback.firstCall.args[1].statusCode, 404);
+      assert.equal(callback.firstCall.args[1].body, "'Not Found'");
+    });
   });
 });
