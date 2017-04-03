cea4560742bbe2f4bac035c12c537c861fe286d1
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-03 18:17:22 +0100

    Add DELETE test to verify there is payload passed or else return 400

--- test/delete.test.js
+++ test/delete.test.js
@@ -9,4 +9,16 @@ describe('Feature flags DELETE endpoint', () => {
       assert.equal(callback.firstCall.args[1].statusCode, 501);
     });
   });
+
+  it('should return 400 when there is no payload', () => {
+      const callback = sandbox.stub();
+      const event = {
+          noBody: JSON.stringify({"featureName": "test1", "state": true})
+      };
+
+      return deleteFlag.handler(event, undefined, callback).catch(() => {
+        assert.equal(callback.firstCall.args[1].statusCode, 400);
+        assert.equal(callback.firstCall.args[1].body, 'Invalid request');
+      });
+  });
 });
