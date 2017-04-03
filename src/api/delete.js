8d9ba4cb3c769d68e61448db057f0402faace0d4
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-03 18:18:13 +0100

    Update the DELETE handler to return 400 if no body passed

--- src/api/delete.js
+++ src/api/delete.js
@@ -1,6 +1,17 @@
 'use strict';
 
 module.exports.handler = (event, context, callback) => {
+  try {
+      payload = JSON.parse(event.body);
+  } catch (e) {
+      return new Promise((resolve, reject) => {
+        callback(null, {
+            "statusCode": 400,
+            "body": "Invalid request"
+        });
+        reject("Invalid request");
+      });
+  }
   return new Promise((resolve, reject) => {
     callback(null, { statusCode: 501, body: "Not Implemented"});
     reject();
