38285e25128cc9cb771ac391f9cc555d8a454d31
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-03 18:21:12 +0100

    Add test: expect DELETE to throw 404 when item we want to delete is not found

--- test/delete.test.js
+++ test/delete.test.js
@@ -1,8 +1,19 @@
 const assert = require('assert');
 const deleteFlag = require('../src/api/delete.js');
+const isEmptyObject = require('../src/lib/is-empty-object');
 const sinon = require('sinon');
+const AWS = require('aws-sdk-mock');
+let sandbox;
 
 describe('Feature flags DELETE endpoint', () => {
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it('should return 501', () => {
     const callback = sinon.stub();
     return deleteFlag.handler(undefined, undefined, callback).catch(() => {
@@ -20,5 +31,19 @@ describe('Feature flags DELETE endpoint', () => {
         assert.equal(callback.firstCall.args[1].statusCode, 400);
         assert.equal(callback.firstCall.args[1].body, 'Invalid request');
       });
+  });
+
+  it('should return 404 when the item is not found in DynamoDB', () => {
+    const callback = sandbox.stub();
+    const event = {
+        body: JSON.stringify({"featureName": "test1"})
+    };
+    AWS.mock('DynamoDB.DocumentClient', 'get', Promise.resolve({}));
+    sandbox.stub(isEmptyObject, 'check').returns(true);
+
+    return deleteFlag.handler(event, undefined, callback).catch(() => {
+      assert.equal(callback.firstCall.args[1].statusCode, 404);
+      assert.equal(callback.firstCall.args[1].body, "Not Found");
+    });
   });
 });
