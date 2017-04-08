447389f4348be3c27e06449f729e7ecd2c0559f5
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-08 10:27:01 +0100

    Update the POST handler test to not rely on the DynamoDB.get to check if the item exists

--- test/post.test.js
+++ test/post.test.js
@@ -19,7 +19,6 @@ describe('Feature flags POST endpoint', () => {
     it('should return 201 when payload is correct and item is not in DynamoDB', () => {
         const callback = sandbox.stub();
         AWS.mock('DynamoDB.DocumentClient', 'put', Promise.resolve());
-        AWS.mock('DynamoDB.DocumentClient', 'get', Promise.resolve({}));
 
         const event = {
             body: JSON.stringify({"featureName": "test1", "state": false})
@@ -36,7 +35,6 @@ describe('Feature flags POST endpoint', () => {
     it('should return 500 when DynamoDB put method fails', () => {
         const callback = sandbox.stub();
         AWS.mock('DynamoDB.DocumentClient', 'put', Promise.reject('Put method error'));
-        AWS.mock('DynamoDB.DocumentClient', 'get', Promise.resolve({}));
         const event = {
             body: JSON.stringify({"featureName": "test1", "state": false})
         };
@@ -49,23 +47,17 @@ describe('Feature flags POST endpoint', () => {
         });
     });
 
-    it('should return 500 when DynamoDB get method fails', () => {
+    it('should return 409 when feature flag is already in DynamoDB', () => {
         const callback = sandbox.stub();
-        AWS.mock('DynamoDB.DocumentClient', 'get', Promise.reject('Get method error'));
-        const event = {
-            body: JSON.stringify({"featureName": "test1", "state": false})
-        };
-        sandbox.stub(isValidRequest, 'validate').returns(true);
-
-        return post.handler(event, undefined, callback).catch(() => {
-          assert.equal(callback.firstCall.args[1].statusCode, 500);
-          assert.equal(callback.firstCall.args[1].body, '"Get method error"');
-        });
-    });
-
-    it('should return 409 when DynamoDB get method find a feature flag entry', () => {
-        const callback = sandbox.stub();
-        AWS.mock('DynamoDB.DocumentClient', 'get', Promise.resolve({ Item: { featureName: 'featureName', state: false } }));
+        AWS.mock('DynamoDB.DocumentClient', 'put', Promise.reject({
+          "message": "The conditional request failed",
+          "code": "ConditionalCheckFailedException",
+          "time": "2017-04-08T08:39:18.125Z",
+          "requestId": "DFBVF8C8908V3RTLCVJ49DPSUNVV4KQNSO5AEMVJF66Q9ASUAAJG",
+          "statusCode": 400,
+          "retryable": false,
+          "retryDelay": 0
+        }));
         const event = {
             body: JSON.stringify({"featureName": "test1", "state": false})
         };
@@ -74,7 +66,6 @@ describe('Feature flags POST endpoint', () => {
 
         return post.handler(event, undefined, callback).catch(() => {
           assert.equal(callback.firstCall.args[1].statusCode, 409);
-          assert.equal(callback.firstCall.args[1].body, '"Feature flag already exists"');
         });
     });
 
