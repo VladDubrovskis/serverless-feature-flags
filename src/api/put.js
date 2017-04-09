54fc35c8e1022c986f827fbb42bdb3f7a21ac4e0
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-09 11:24:15 +0100

    Update PUT handler to add the Expected property when we update item to database instead of checking ourselves We are checking if item exists before we update it

--- src/api/put.js
+++ src/api/put.js
@@ -23,34 +23,30 @@ module.exports.handler = (event, context, callback) => {
 
   const updateItemParams = {
       "TableName": "featureFlags",
-      "Item": updateItem
-  };
-
-  const checkItemParams = {
-      "TableName": "featureFlags",
-      "Key": {
-        "featureName": payload.featureName,
+      "Item": updateItem,
+      "Expected": {
+          "featureName": {
+              "Exists": true,
+              "Value": payload.featureName
+          }
       }
   };
+
   const docClient = new aws.DynamoDB.DocumentClient();
 
   return new Promise((resolve, reject) => {
-    docClient.get(checkItemParams).promise()
-      .then((item) => {
-        if(isEmptyObject.check(item)) {
-          callback(null, { "statusCode": 404, "body": "Not Found"});
-          reject();
-        } else {
-          return docClient.put(updateItemParams).promise()
-            .then(() => {
-                callback(null, {"statusCode": 204});
-                resolve();
-            })
-        }
-      })
-      .catch((err) => {
-        callback(null, {"statusCode": 500, "body": JSON.stringify(err)});
-        reject(err);
-      })
+      return docClient.put(updateItemParams).promise()
+        .then(() => {
+            callback(null, {"statusCode": 204});
+            resolve();
+        })
+        .catch((err) => {
+          let statusCode = 500;
+          if(err.statusCode && err.statusCode === 400) {
+            statusCode = 404;
+          }
+          callback(null, {statusCode, "body": JSON.stringify(err)});
+          reject(err)
+        });
   });
 };
