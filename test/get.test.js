eb36ce639f9cfd1be433ac3607468f9b73e89220
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-16 01:27:09 +0100

    Update GET handler test to use the storage module

--- test/get.test.js
+++ test/get.test.js
@@ -1,39 +1,41 @@
 const assert = require('assert');
 const get = require('../src/api/get.js');
-const AWS = require('aws-sdk-mock');
 const sinon = require('sinon');
+const storage = require('../src/lib/storage');
 const responseTransform = require('../src/lib/response-transform');
+
+let sandbox;
 
 describe('Feature flags GET endpoint', () => {
   let responseTransformResponse;
-  let responseTransformStub;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
 
   afterEach(() => {
-    AWS.restore();
-    responseTransformStub.reset();
+    sandbox.restore();
   });
 
   it('should return 200 with data from DynamoDB', () => {
-    AWS.mock('DynamoDB.DocumentClient', 'scan', Promise.resolve({}));
-
     responseTransformResponse = {
       test2: true,
       test: false,
     };
 
-    responseTransformStub = sinon.stub(responseTransform, 'transform').returns(responseTransformResponse);
+    sandbox.stub(storage, 'get').returns(Promise.resolve({ Items: {} }));
+    sandbox.stub(responseTransform, 'transform').returns(responseTransformResponse);
 
-    const callback = sinon.stub();
+    const callback = sandbox.stub();
     return get.handler(undefined, undefined, callback).then(() => {
       assert.equal(callback.firstCall.args[1].statusCode, 200);
-      assert.equal(responseTransformStub.callCount, 1);
       assert.equal(callback.firstCall.args[1].body, JSON.stringify(responseTransformResponse));
     });
   });
 
   it('should return 500 when read from DynamoDB fails', () => {
-    AWS.mock('DynamoDB.DocumentClient', 'scan', Promise.reject('Scan method error'));
-    const callback = sinon.stub();
+    const callback = sandbox.stub();
+    sandbox.stub(storage, 'get').returns(Promise.reject('Scan method error'));
     return get.handler(undefined, undefined, callback).catch(() => {
       assert.equal(callback.firstCall.args[1].statusCode, 500);
       assert.equal(callback.firstCall.args[1].body, '"Scan method error"');
