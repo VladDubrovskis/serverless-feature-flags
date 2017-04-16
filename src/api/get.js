dcf33ab8df6c3c5325dea3c270cd3104b886d3a8
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-16 01:27:31 +0100

    Update GET handler to use the storage module

--- src/api/get.js
+++ src/api/get.js
@@ -1,19 +1,14 @@
-const aws = require('aws-sdk');
+const storage = require('../lib/storage');
 const responseTransform = require('../lib/response-transform');
 
 module.exports.handler = (event, context, callback) => {
-  const params = {
-    TableName: 'featureFlags',
-  };
-
-  const docClient = new aws.DynamoDB.DocumentClient();
   const response = {
     statusCode: 200,
   };
 
 
   return new Promise((resolve, reject) => {
-    docClient.scan(params).promise()
+    storage.get()
       .then((data) => {
         response.body = JSON.stringify(responseTransform.transform(data.Items));
         callback(null, response);
