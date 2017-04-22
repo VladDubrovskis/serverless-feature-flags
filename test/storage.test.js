30b3db25d77de869d310eb6507127a55a199b4a3
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-22 10:14:21 +0100

    Update storage tests to verify that the AWS SDK methods have been called

--- test/storage.test.js
+++ test/storage.test.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk-mock');
 const storage = require('../src/lib/storage');
+const assert = require('assert');
 
 describe('The storage module', () => {
   afterEach(() => {
@@ -7,22 +8,25 @@ describe('The storage module', () => {
   });
 
   it('should have get method that returns a promise', (done) => {
-    AWS.mock('DynamoDB.DocumentClient', 'scan', Promise.resolve({}));
+    const dynamoDBMock = AWS.mock('DynamoDB.DocumentClient', 'scan', Promise.resolve({}));
     storage.get().then(() => {
+      assert.equal(dynamoDBMock.stub.calledOnce, true);
       done();
     });
   });
 
   it('should have put method that returns a promise', (done) => {
-    AWS.mock('DynamoDB.DocumentClient', 'put', Promise.resolve({}));
+    const dynamoDBMock = AWS.mock('DynamoDB.DocumentClient', 'put', Promise.resolve({}));
     storage.put().then(() => {
+      assert.equal(dynamoDBMock.stub.calledOnce, true);
       done();
     });
   });
 
   it('should have update method that returns a promise', (done) => {
-    AWS.mock('DynamoDB.DocumentClient', 'update', Promise.resolve({}));
+    const dynamoDBMock = AWS.mock('DynamoDB.DocumentClient', 'update', Promise.resolve({}));
     storage.update().then(() => {
+      assert.equal(dynamoDBMock.stub.calledOnce, true);
       done();
     });
   });
@@ -30,6 +34,7 @@ describe('The storage module', () => {
   it('should have delete method that returns a promise', (done) => {
     const dynamoDBMock = AWS.mock('DynamoDB.DocumentClient', 'delete', Promise.resolve({}));
     storage.delete().then(() => {
+      assert.equal(dynamoDBMock.stub.calledOnce, true);
       done();
     });
   });
