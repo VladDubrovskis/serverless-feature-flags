388f4ed45262166e135c05ab08395e3ea150c9f9
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-27 10:31:01 +0100

    Update the generic handler to have a payload validation - Return 400 when invalid - Reutrn 203 when all is good

--- src/lib/handler.js
+++ src/lib/handler.js
@@ -1,3 +1,26 @@
+const isValidRequest = require('../lib/is-valid-request');
+
 module.exports = {
-  execute: () => Promise.resolve(true),
+  execute: (event, context, callback) => {
+    const payload = isValidRequest.validate(event.body);
+    if (payload === false) {
+      return new Promise((resolve, reject) => {
+        callback(null, {
+          statusCode: 400,
+          body: {
+            error: {
+              code: 400,
+              message: 'Invalid request',
+            },
+          },
+        });
+        reject('Invalid request');
+      });
+    }
+
+    return new Promise((resolve) => {
+      callback(undefined, { statusCode: 203 });
+      resolve();
+    });
+  },
 };
--- test/handler.test.js
+++ test/handler.test.js
@@ -1,11 +1,38 @@
 const assert = require('assert');
 const handler = require('../src/lib/handler.js');
+const isValidRequest = require('../src/lib/is-valid-request');
+const sinon = require('sinon');
+
+let sandbox;
 
 describe('Lambda handler', () => {
-  it('should return true', (done) => {
-    handler.execute().then((result) => {
-      assert.equal(result, true);
-      done();
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should resolve with a 203 on succesfull call', () => {
+    const callback = sandbox.stub();
+    sandbox.stub(isValidRequest, 'validate').returns(true);
+    return handler.execute({}, undefined, callback).then(() => {
+      assert.equal(callback.firstCall.args[1].statusCode, 203);
+    });
+  });
+
+  it('should return 400 when the payload is invalid', () => {
+    const callback = sandbox.stub();
+    sandbox.stub(isValidRequest, 'validate').returns(false);
+    return handler.execute({}, undefined, callback).catch(() => {
+      assert.equal(callback.firstCall.args[1].statusCode, 400);
+      assert.deepEqual(callback.firstCall.args[1].body, {
+        error: {
+          code: 400,
+          message: 'Invalid request',
+        },
+      });
     });
   });
 });
