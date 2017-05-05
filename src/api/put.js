f4b436016e7825d32d195d3171919fadb68bfd68
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-05-05 08:48:46 +0100

    Update the PUT handler to make us of the generic one

--- src/api/put.js
+++ src/api/put.js
@@ -1,43 +1,5 @@
 const storage = require('../lib/storage');
-const isValidRequest = require('../lib/is-valid-request');
+const handler = require('../lib/handler');
 
-module.exports.handler = (event, context, callback) => {
-  const payload = isValidRequest.validate(event.body);
-  if (payload === false) {
-    return new Promise((resolve, reject) => {
-      callback(null, {
-        statusCode: 400,
-        body: {
-          error: {
-            code: 400,
-            message: 'Invalid request',
-          },
-        },
-      });
-      reject('Invalid request');
-    });
-  }
-
-  return new Promise((resolve, reject) => storage.update(payload.featureName, payload.state)
-      .then(() => {
-        callback(null, { statusCode: 204 });
-        resolve();
-      })
-      .catch((err) => {
-        let statusCode = 500;
-        if (err.statusCode && err.statusCode === 400) {
-          statusCode = 404;
-        }
-        callback(null,
-          {
-            statusCode,
-            body: JSON.stringify({
-              error: {
-                code: statusCode,
-                message: err,
-              },
-            }),
-          });
-        reject(err);
-      }));
-};
+module.exports.handler = (event, context, callback) =>
+  handler.execute(storage.update, event, context, callback, 204, { 400: 404 });
--- test/put.test.js
+++ test/put.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const put = require('../src/api/put.js');
-const isValidRequest = require('../src/lib/is-valid-request');
 const sinon = require('sinon');
+const handler = require('../src/lib/handler');
 const storage = require('../src/lib/storage');
 
 let sandbox;
@@ -15,50 +15,13 @@ describe('Feature flags PUT endpoint', () => {
     sandbox.restore();
   });
 
-  it('should return 204 when payload is correct and item is found in database', () => {
+  it('should use generic handler and pass the storage.update as method', () => {
     const callback = sandbox.stub();
-    const payload = { featureName: 'test1', state: false };
-    sandbox.stub(isValidRequest, 'validate').returns(payload);
-    const storageStub = sandbox.stub(storage, 'update').returns(Promise.resolve());
-
-    return put.handler({}, undefined, callback).then(() => {
-      assert.equal(callback.firstCall.args[1].statusCode, 204);
-      assert.equal(storageStub.calledWith(payload.featureName, payload.state), true);
-    });
-  });
-
-  it('should return 404 when the item is not found in database', () => {
-    const callback = sandbox.stub();
-    sandbox.stub(isValidRequest, 'validate').returns(true);
-    sandbox.stub(storage, 'update').returns(Promise.reject({ statusCode: 400 }));
-
-    return put.handler({}, undefined, callback).catch(() => {
-      assert.equal(callback.firstCall.args[1].statusCode, 404);
-    });
-  });
-
-  it('should return 500 when database update method fails', () => {
-    const callback = sandbox.stub();
-    sandbox.stub(isValidRequest, 'validate').returns(true);
-    sandbox.stub(storage, 'update').returns(Promise.reject('Update method error'));
-
-    return put.handler({}, undefined, callback).catch(() => {
-      assert.equal(callback.firstCall.args[1].statusCode, 500);
-      assert.equal(callback.firstCall.args[1].body, '{"error":{"code":500,"message":"Update method error"}}');
-    });
-  });
-
-  it('should return 400 when the payload is invalid', () => {
-    const callback = sandbox.stub();
-    sandbox.stub(isValidRequest, 'validate').returns(false);
-    return put.handler({}, undefined, callback).catch(() => {
-      assert.equal(callback.firstCall.args[1].statusCode, 400);
-      assert.deepEqual(callback.firstCall.args[1].body, {
-        error: {
-          code: 400,
-          message: 'Invalid request',
-        },
-      });
+    const handlerStub = sandbox.stub(handler, 'execute').returns(Promise.resolve());
+    const context = { context: 1 };
+    const event = {};
+    return put.handler(event, context, callback).then(() => {
+      assert(handlerStub.calledWith(storage.update, event, context, callback), true);
     });
   });
 });
