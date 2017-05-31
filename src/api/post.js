1017fceaa13770471379f1912ccc4f50d3f116cf
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-05-31 07:46:51 +0100

    Finish off handler refacctor - all files to use the exported functions instead of objects with “.execute”

--- src/api/post.js
+++ src/api/post.js
@@ -2,6 +2,6 @@ const storage = require('../lib/storage');
 const handler = require('../lib/handler');
 
 module.exports.handler = (event, context, callback) =>
-  handler.execute(storage.put, event, context, 204, { 400: 409 })
-      .then((result) => callback(null, result))
-      .catch((error) => callback(null, error));
+  handler(storage.put, event, context, 204, { 400: 409 })
+      .then(result => callback(null, result))
+      .catch(error => callback(null, error));
--- src/api/put.js
+++ src/api/put.js
@@ -2,6 +2,6 @@ const storage = require('../lib/storage');
 const handler = require('../lib/handler');
 
 module.exports.handler = (event, context, callback) =>
-  handler.execute(storage.update, event, context, 204, { 400: 404 })
-      .then((result) => callback(null, result))
-      .catch((error) => callback(null, error));
+  handler(storage.update, event, context, 204, { 400: 404 })
+      .then(result => callback(null, result))
+      .catch(error => callback(null, error));
